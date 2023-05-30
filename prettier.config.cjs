d052f9c5ba3fb6f22bbada53b70c27f92df9df62
Darius Pasca <darius.pasca@bitrock.it>
2023-05-30 11:46:17 +0200

    add: @ianvs/prettier-plugin-sort-imports

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,34 @@
-/** @type {import("prettier").Config} */
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^~/env(.*)$",
+    "^~/types/(.*)$",
+    "^~/config/(.*)$",
+    "^~/lib/(.*)$",
+    "^~/hooks/(.*)$",
+    "^~/components/ui/(.*)$",
+    "^~/components/(.*)$",
+    "^~/styles/(.*)$",
+    "^~/app/(.*)$",
+    "",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.0.0",
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("@ianvs/prettier-plugin-sort-imports"),
+  ],
+}
 
-module.exports = config;
+module.exports = config
